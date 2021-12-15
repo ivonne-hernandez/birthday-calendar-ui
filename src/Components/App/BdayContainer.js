818c5e2c3da41ad86d6595b13cfa7f2948b04718
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-15 10:41:04 -0700

    Refactored the BdayContainer elements so that the birthdays are inside of their corresponding month

--- src/Components/App/BdayContainer.js
+++ src/Components/App/BdayContainer.js
@@ -1,9 +1,21 @@
 
-const BdayContainer = ({ months }) => {
+const BdayContainer = ({ months, birthdays }) => {
   const bdayMonths = months.map(month => {
+    const matchingBirthdaysForThatMonth = birthdays.filter(birthday => birthday.month === month.id);
+
     return (
       <div key={month.id} className="bday-month-container">
         <h3 className="month-name">{month.name}</h3>
+        {matchingBirthdaysForThatMonth.length ? 
+          <div>
+           {
+              matchingBirthdaysForThatMonth.map(matchingBday => {
+                return (
+                  <p key={matchingBday.id}>{matchingBday.name}: {matchingBday.month}/{matchingBday.day}</p>
+                )
+              })
+           } 
+          </div> : <p className="no-birthdays-message">No birthdays this month</p>}
       </div>
     )
   });
