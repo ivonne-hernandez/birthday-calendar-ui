c8572665091259ef601b6cdb3fb01f26d63e7924
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-15 10:29:24 -0700

    Import getAllBdays fetch function, add a componentDidMount method, and pass the birthdays as props to the BdayContainer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import { months } from '../../months_data';
 import { Component } from 'react';
 import BdayContainer from './BdayContainer';
+import { getAllBdays } from './apiCalls';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      months: months
+      months: months,
+      birthdays :[]
     }
+  }
+
+  componentDidMount = () => {
+    return getAllBdays()
+      .then(data => {
+        this.setState({birthdays: data})
+      })
   }
 
   render = () => {
@@ -18,6 +27,7 @@ class App extends Component {
         <div className='bday-form'/>
         <BdayContainer 
           months={this.state.months}
+          birthdays={this.state.birthdays}
           className='bday-container'/>
       </div>
     )
