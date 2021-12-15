abc6bd0216d51b1bc6082243fb666f5e3336eb00
Ivonne Hernandez <ivonne702@gmail.com>
2021-12-15 10:44:37 -0700

    Add a new postNewBday function to apiCalls

--- src/Components/App/apiCalls.js
+++ src/Components/App/apiCalls.js
@@ -3,7 +3,19 @@ const getAllBdays = () => {
     .then(response => response.json())
 }
 
+const postNewBday = (newBday) => {
+  return fetch('http://localhost:3001/api/v1/birthdays', {
+    method: 'POST',
+    body: JSON.stringify(newBday),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+}
+
 
 module.exports = {
-  getAllBdays
+  getAllBdays,
+  postNewBday
 }
